ecb86f239ace54203b31a28d87e292eec591f1f9
Karolis Strazdas <karolis.strazdas@pm.me>
2021-06-06 01:30:05 +0300

    Init.

--- dev/net-bots-client/src/index.ts
+++
@@ -1,7 +0,0 @@
-import { define } from 'hybrids';
-import { SimpleCounter } from "./components/counter";
-
-if (process.env.NODE_ENV !== 'production')
-    module.hot?.accept();
-
-define('simple-counter', SimpleCounter);
