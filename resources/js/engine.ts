0dc8e17ba644b2e3fb3a51107344d4153aeae913
Eoic <karolis.strazdas@protonmail.com>
2021-09-13 00:54:41 +0300

    Remove moveable system from world instance.

--- resources/js/engine.ts
+++ resources/js/engine.ts
@@ -1,8 +1,8 @@
 import { World } from 'ecsy';
 import * as PIXI from 'pixi.js';
-import { Application, IApplicationOptions, Ticker } from 'pixi.js';
-import { Position, Renderable, Velocity, Interactable, BotController } from './core/components';
-import { MoveableSystem, RendererSystem, InteractableSystem } from './core/systems';
+import { Application, IApplicationOptions, Sprite, Ticker } from 'pixi.js';
+import { Position, Renderable, Velocity, Interactable } from './core/components';
+import { RendererSystem, InteractableSystem } from './core/systems';
 import { InputManager } from './core/managers/input-manager';
 import { Camera } from './rendering/camera';
 import { Map } from './utils/map';
@@ -21,8 +21,8 @@ export class Engine {
         this.ticker = PIXI.Ticker.shared;
         this.ticker.autoStart = false;
         this.world = new World();
-        this.registerComponents([Position, Renderable, Velocity, Interactable, BotController]);
-        this.registerSystems([MoveableSystem, RendererSystem, InteractableSystem]);
+        this.registerComponents([Position, Renderable, Velocity, Interactable]);
+        this.registerSystems([RendererSystem, InteractableSystem]);
         this.root!.appendChild(this.app.view);
         this.mainCamera = new Camera(this.root!);
         this.handleEvents();
@@ -36,13 +36,9 @@ export class Engine {
             isDragEnabled: true,
             isZoomEnabled: false,
         });
+
         this.app.stage.addChild(this.map);
-        
-        // this.createPlayerEntity(this.map, { x: 768, y: 512 });
-        // InputManager.instance.addKey(Keys.W);
-        // InputManager.instance.addKey(Keys.A);
-        // InputManager.instance.addKey(Keys.S);
-        // InputManager.instance.addKey(Keys.D);
+        this.createPlayerEntity(this.map, { x: 768, y: 512 });
     }
 
     private handleEvents() {
@@ -71,26 +67,25 @@ export class Engine {
         });
     }
 
-    // private createPlayerEntity(parent, position) {
-    //     const sprite = this.getSprite(parent);
+    private createPlayerEntity(parent, position) {
+        const sprite = this.getSprite(parent);
 
-    //     this.world
-    //         .createEntity()
-    //         .addComponent(Velocity, { x: 5, y: 5 })
-    //         .addComponent(Position, { x: position.x, y: position.y })
-    //         .addComponent(Renderable, { sprite, width: sprite.width, height: sprite.height })
-    //         .addComponent(Interactable)
-    //         .addComponent(BotController, { speed: 0.2 });
-    // }
+        this.world
+            .createEntity()
+            .addComponent(Velocity, { x: 5, y: 5 })
+            .addComponent(Position, { x: position.x, y: position.y })
+            .addComponent(Renderable, { sprite, width: sprite.width, height: sprite.height })
+            .addComponent(Interactable)
+    }
 
-    // private getSprite(parent) {
-    //     const graphics = new PIXI.Graphics();
-    //     graphics.beginFill(0x4073ff);
-    //     graphics.drawRect(0, 0, 64, 64);
-    //     graphics.endFill();
-    //     const texture = this.app.renderer.generateTexture(graphics);
-    //     const sprite = new Sprite(texture);
-    //     parent.addChild(sprite);
-    //     return sprite;
-    // }
+    private getSprite(parent) {
+        const graphics = new PIXI.Graphics();
+        graphics.beginFill(0x4073ff);
+        graphics.drawRect(0, 0, 64, 64);
+        graphics.endFill();
+        const texture = this.app.renderer.generateTexture(graphics);
+        const sprite = new Sprite(texture);
+        parent.addChild(sprite);
+        return sprite;
+    }
 }
