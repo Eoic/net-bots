14356ce9a5f0eb09c60d7e88aed097ed82def97d
Karolis Strazdas <karolis.strazdas@indeform.com>
2022-05-22 18:20:18 +0300

    Cleanup for different project configuration.

--- TODO.md
+++
@@ -1,1 +0,0 @@
-* Responsive navigation bar.
--- src/pages/Documentation.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const Documentation = () => {
-    return (
-        <div className='fill-height'>
-            Documentation
-        </div>
-    )
-}
